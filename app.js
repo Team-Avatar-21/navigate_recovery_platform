cd83e88b3cdaa56fb9f0608358c48948738f035c
Bryan Dao <bdao@ggc.edu>
2021-03-10 13:07:14 -0500

    added test file/dir

---
+++ app.js
@@ -0,0 +1,8 @@
+module.exports = { // makes what ever we put here accessible outside of the file
+    sayHello: function(){
+        return 'hello';
+    },
+    addNumbers: function(value1, value2){
+        return value1 + value2;
+    }
+}
