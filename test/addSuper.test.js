7ae412338a5349827cf6915e0da35f2927322ddf
Bryan Dao <bdao@ggc.edu>
2021-04-03 14:13:21 -0400

    remove package-lock.json and update test case

--- test/addSuper.test.js
+++ test/addSuper.test.js
@@ -2,40 +2,63 @@
 const { expect } = require('chai');
 
 // bring in mock library
-const { mockRequest, mockResponse} = require('mock-req-res');
-
-// mock firebase admin
-jest.mock("firebase-admin", () => {
-    return {
-        apps: 1,
-        auth: jest.fn(createUser => jest.fn(uid)),
-    };
-});
-jest.mock("../navigate-recovery-platfom-firebase-adminsdk-r5iv4-ea3204fe8f.json", () => ({
-    settings: 'this is a test'
-}), {virtual: true});
-
+const httpMocks = require('node-mocks-http');
 
 // bring in super user file to test
 const superuser = require('../pages/api/user/add_superuser');
 
-const req = mockRequest({body: {email:'test.gmail.com', password:'test1', displayName:'test account'}});
-const res = mockResponse();
+// mock empty file that the createSuperUser module imports
+jest.mock("../navigate-recovery-platfom-firebase-adminsdk-r5iv4-ea3204fe8f.json", () => ({ 
+    settings: 'this is a test'
+}), {virtual: true});
 
+// mock firebase admin and stub the necessary functions that the createSuperUser module calls
+jest.mock("firebase-admin", () => {
+    return {
+        apps: 1,
+        auth: function() {
+            return {
+                createUser: function() {
+                    return Promise.resolve({ uid: 'fake_user_id', this: 'is_as_test' });
+                },
+                setCustomUserClaims: async function(uid, customClaims) {
+                    return null;
+                },
+                getUserByEmail: function(email) {
+                    return { uid: 'fake_user_id', this: 'is_as_test' }
+                }
+            }
+        },
+    };
+});
+
+// create the mock request and and response for the test
+const req = httpMocks.createRequest({
+    body: {
+        email:'test.gmail.com',
+        password: 'test1',
+        displayName: 'test account'
+    }
+});
+
+const res = httpMocks.createResponse();
+
+// set test conditions
 beforeEach(() => {
-    superuser.createSuperUser(req, res);
+    // call the create super user with the mocked params
+    superuser.createSuperUser(req,res);
 });
 
 afterEach(() => {
-   
-    // need to delete (test.gmail.com) super user to clean up after testing 
+
 });
 
 describe('api/user', () => {
     describe('add super user', () => {
-        it ('should create new user', () => {
-            console.info(res.json);
-            // expect(res.json);
+        it('should create new user', () => {
+            // expect the response to have uid which means the user was created in firebase
+            expect(res.getData()).to.have.property("uid")
         });
     });
-})
+});
+
