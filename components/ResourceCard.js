001a82bed442943aeb2e995c5dfa1661c1cb31a8
Bryan Dao <bdao@ggc.edu>
2021-04-11 14:12:41 -0400

    add see attributes on click

--- components/ResourceCard.js
+++ components/ResourceCard.js
@@ -2,66 +2,119 @@ import { Grid, Typography, Button, Card } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { useState } from "react";
+import { useAuth } from "../utils/auth";
+import fetch from "../utils/fetch";
 
 /**
 * Component class that represents a resource card
 */
 const useStyles = makeStyles((theme) => {
-return {
+  return {
 card: {
 minWidth: " 150px",
 minHeight: "150px",
 padding: "10px",
 },
-};
+    };
 });
 
 export default function ResourceCard({ resources, attrs, onDelete }) {
-const classes = useStyles();
-const [isShown, setIsShown] = useState(false);
+  const classes = useStyles();
+  const [isShown, setIsShown] = useState(false);
+  const [moreInfo, setMoreInfo] = useState({});
+  const auth = useAuth();
 
-/**
-* Prepares data for a resource card
-* simply displays each attribute with appropriate value
-*/
-const cardData = Object.keys(resources).map((key) => {
-const attribute_name = attrs[key];
-return (
-<Typography key={key}>
-{attribute_name}: {String(resources[key])}
-</Typography>
-);
-});
+  const getResourceQuery = (organizationName) => {
+    return {
+      query: `query GET_RESOURCE {
+        Resources (where: {organizationName: {_eq: "` + organizationName + `"}}) {
+          organizationName
+          id
+          City
+          CocaineTreatment
+          acceptHealthIns
+          allowsCouples
+          availableScholarships
+          canWork
+          coEd
+          detox
+          drugCourtApproved
+          freeServices
+          friendlyLGBT
+          homeless
+          homelessWomen
+          inPatient
+          transFriendly
+          sexTraffickingVictims
+          providesTransportation
+          privatelyOwned
+          outPatient
+          methTreatment
+          womenOnly
+          womenWithChildren
+        }
+      }`,
+    }
+  }
 
-function toggleInfo() {
-if (isShown) {
-setIsShown(false);
-} else {
-setIsShown(true);
-}
-}
+  /*
+    Gets resource data
+  */
+    const handleGetResourceData = async (organizationName) => {
+      const data = await fetch(getResourceQuery(organizationName), auth.authState.tokenResult.token)
+      .then((data) => {
+        setMoreInfo(data.Resources[0]);
+        setIsShown(true);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+    };
 
-function MoreInfo(props) {
-if (isShown) {
-return (
-<div>Testing</div>
-);
-} else {
-return (
-<div></div>
-);
-}
-}
+  /**
+  * Prepares data for a resource card
+  * simply displays each attribute with appropriate value
+  */
+  const cardData = Object.keys(resources).map((key) => {
+  const attribute_name = attrs[key];
+    return (
+      <Typography key={key}>
+        {attribute_name}: {String(resources[key])}
+      </Typography>
+    );
+  });
 
-return (
-<Card className={classes.card} onClick={() => toggleInfo()}>
-{cardData}{" "}
-<MoreInfo></MoreInfo>
-{onDelete ? (
-<DeleteIcon onClick={() => onDelete(resources.organizationName)} />
-) : (
-""
-)}
-</Card>
-);
+  function MoreInfo() {
+
+    if (isShown) {
+      return (
+        <div>
+          {
+            Object.keys(moreInfo).map((key) => {
+              return <Typography key={key}>{key}: {moreInfo[key]}</Typography>;
+            })
+          }
+          <a onClick={() => setIsShown(false)}>See Less</a>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <a onClick={() => handleGetResourceData(resources["organizationName"])}>See More</a>
+        </div>
+      );
+    }
+  }
+
+  return (
+    <Card className={classes.card}>
+      {cardData}{" "}
+      <MoreInfo></MoreInfo>
+      {onDelete ? (
+        <DeleteIcon onClick={() => onDelete(resources.organizationName)} />
+      ) : (
+        ""
+      )}
+    </Card>
+  );
 }
