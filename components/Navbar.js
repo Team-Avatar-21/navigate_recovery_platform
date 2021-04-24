9b4e38e445a400851d522762ce6ab6025c6b5f4a
Kevin <kkim9@ggc.edu>
2021-04-24 17:34:57 -0400

    k

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,6 +6,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
